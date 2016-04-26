c052d81669f7a793268712091d888f0485dc947a
slifyod <yunus.kara@tu.univ-tours.fr>
2016-04-26 15:10:30 +0200

    xml of Author,Book,Type and User         corrigé

--- src/com/polytech/di4/si/archi/model/Author.java
+++ src/com/polytech/di4/si/archi/model/Author.java
@@ -5,5 +5,23 @@ package com.polytech.di4.si.archi.model;
  */
 public class Author {
     private long id;
+
     private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/com/polytech/di4/si/archi/model/Book.java
+++ src/com/polytech/di4/si/archi/model/Book.java
@@ -10,4 +10,36 @@ public class Book {
     private String name;
     private Type type;
     private List<Author> authors;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
 }
