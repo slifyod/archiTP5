f02c8c6bb142be544ff8dfe30b8de9b8466a4c1e
Timothy <timothy.nibeaudeau@etu.univ-tours.fr>
2016-04-26 15:17:11 +0200

    Add model

---
+++ src/com/polytech/di4/si/archi/filter/AuthentificationFilter.java
@@ -0,0 +1,7 @@
+package com.polytech.di4.si.archi.filter;
+
+/**
+ * Created by linux on 26/04/16.
+ */
+public class AuthentificationFilter{
+}
---
+++ src/com/polytech/di4/si/archi/model/Borrow.java
@@ -0,0 +1,13 @@
+package com.polytech.di4.si.archi.model;
+
+import java.util.Date;
+
+/**
+ * Created by linux on 26/04/16.
+ */
+public class Borrow {
+    private long id;
+    private User user;
+    private Book book;
+    private Date deadline;
+}
